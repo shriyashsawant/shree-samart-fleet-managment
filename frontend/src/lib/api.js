2c6c9c9d728c3b94b850939e07285ff3e1dc348c
shriyashsawant <shriyashsantoshsawant@gmail.com>
2026-03-19 15:44:42 +0530

    Fix all lazy loading and CORS issues and Backend URL

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'https://shree-samart-fleet-managment.onrender.com'
+const API_URL = 'https://shreesamarth-backend.onrender.com'
 
 const api = axios.create({
   baseURL: API_URL,
